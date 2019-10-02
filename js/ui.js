b050b846fe971e2e34d8843275c678efe3e98b8c
joao vitor <resetgamer1@gmail.com>
2019-10-01 21:31:36 -0300

    Added a cool message for diferents times

--- js/ui.js
+++ js/ui.js
@@ -5,26 +5,32 @@ class UI {
     }
     
     updateWeather(data) {
+            var coolMessage = '';
             var icon='';
             if(data.weather.weather[0].main=='Haze')
-                icon='fas fa-cloud';
+                icon='fas fa-cloud'
+                coolMessage = " - Maybe we inside the clouds 🙃";
             if(data.weather.weather[0].main=='Clear')
+                coolMessage = " - The day is beautiful 😀"
                 icon= 'fas fa-sun';
             if(data.weather.weather[0].main=='Rain')
+                coolMessage = " - A great day to watch netflix 😴"
                 icon='fas fa-cloud-showers-heavy';
             if(data.weather.weather[0].main=='Snow')
+                coolMessage = " - Lets make a snowman ☃️"
                 icon= 'far fa-snowflake';  
             if(data.weather.weather[0].main=='Mist')
+                coolMessage = " - Don't forget your coat 🧥"
                 icon= 'fas fa-smog';        
 
         console.log(data.weather.name,icon);
         var output = ``;
         output += `
         <h1 class="text-dark" id="w-location">${data.weather.name}</h1>
-        <h3 class="text-dark mb-3" id="w-desc">${data.weather.weather[0].main}</h3>
+        <h3 class="text-dark mb-3" id="w-desc">${data.weather.weather[0].main + coolMessage}</h3>
         <i class="${icon}"></i><br>
         <div class=" list-group" id="list">
-            <div class="list-group-item"><b>Temperature</b>: ${data.weather.main.temp} C </div>
+            <div class="list-group-item"><b>Temperature</b>: ${data.weather.main.temp } C </div>
             <div class="list-group-item"><b>Max Temperature:</b> ${data.weather.main.temp_max} C</div>
             <div class="list-group-item"><b>Min Temperature:</b> ${data.weather.main.temp_min} C</div>
             <div class="list-group-item"><b>Humidity(in %)</b>: ${data.weather.main.humidity}</div>
